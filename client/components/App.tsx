6ab030d70fa956a45907ab3869613a689d085400
Timothy itayi <timmytam10@gmail.com>
2023-03-17 10:12:12 +1300

    rendered Auth and Feed

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,14 @@
+import Auth from './Auth'
+import Feed from './Feed'
+
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+    <Auth />
+    <Feed />
+
     </div>
   )
 }
