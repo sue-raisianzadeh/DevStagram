98eac937670194a2e0d03346407af87b23ce293f
Timothy itayi <timmytam10@gmail.com>
2023-03-17 10:28:27 +1300

    added logo

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,12 +3,13 @@ import Feed from './Feed'
 
 function App() {
   return (
-    <div>
+    <div className="title">
+      <img src="server/public/images/group 3 logo.jpg" alt="DevStagram logo" />
+
       <h1>App</h1>
       <p>React development has begun!</p>
-    <Auth />
-    <Feed />
-
+      <Auth />
+      <Feed />
     </div>
   )
 }
