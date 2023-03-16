4582a93c48e298233590564f4008749bb394579a
sue raisianzadeh <raisianz@gmail.com>
2023-03-17 10:27:15 +1300

    auth page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,17 @@
+import { Router } from 'express'
+import React from 'react'
+import Auth from './Auth'
+import { Router } from 'react-router-dom'
+
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+
+      <Router>
+        <Auth />
+      </Router>
     </div>
   )
 }
--- client/components/Auth.tsx
+++ client/components/Auth.tsx
@@ -1,8 +1,28 @@
-
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Auth = () => {
+  const [input, setInput] = useState({
+    userName: '',
+    Password: '',
+  })
   return (
-    <div>Auth</div>
+    <div>
+      <form className="text-black flex flex-col">
+        <label htmlFor="userName">User name</label>
+        <input type="text" name="user" id="userName" onClick={handleChange} />
+        <label htmlFor="author">Password</label>
+        <input
+          type="password"
+          name="author"
+          id="password"
+          onChange={handleChange}
+        />
+
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
   )
 }
 
